43fa5bc76cb87f601b6dade5eed629124cb8983b
zclilin615 <15585887548@163.com>
2024-10-31 17:39:52 +0800

    test

--- 2024pr2/src/main.c
+++ 2024pr2/src/main.c
@@ -48,11 +48,7 @@ float *Matrix_mul(float *mat1, size_t row1, size_t col1, float *mat2, size_t row
 {
     if (mat1 == NULL || mat2 == NULL)
         sprintf(stderr, "Matrix is NULL.\n");
-    if (col1 != row2)//矩阵乘法的条件,用col1或者row2当作迭代数
+    if (col1 != row2) // 矩阵乘法的条件,用col1或者row2当作迭代数
         sprintf(stderr, "Matrix dimensions do not match\n");
-    float *result = Matrix_create(row1, col2);
-    
-   
-
- 
+    float *result = Matrix_create(row1, col2);//哈哈哈
 }
